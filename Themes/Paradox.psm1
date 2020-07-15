817ae83f7ee7111edcb3d55be0a67bb54f0b5e24
Eisbehr <git@eisbehr.de>
2020-07-15 19:19:04 +0200

    fix display issue of vcs icon in powershell
    Added an invisible unicode space character in front of the vcs/gihub icon to fix display issue in powershell.

--- Themes/Paradox.psm1
+++ Themes/Paradox.psm1
@@ -45,7 +45,7 @@ function Write-Theme {
         $themeInfo = Get-VcsInfo -status ($status)
         $lastColor = $themeInfo.BackgroundColor
         $prompt += Write-Prompt -Object $($sl.PromptSymbols.SegmentForwardSymbol) -ForegroundColor $sl.Colors.PromptBackgroundColor -BackgroundColor $lastColor
-        $prompt += Write-Prompt -Object " $($themeInfo.VcInfo) " -BackgroundColor $lastColor -ForegroundColor $sl.Colors.GitForegroundColor
+        $prompt += Write-Prompt -Object "$($sl.PromptSymbols.UnicodeSpace)$($themeInfo.VcInfo) " -BackgroundColor $lastColor -ForegroundColor $sl.Colors.GitForegroundColor
     }
 
     # Writes the postfix to the prompt
@@ -71,6 +71,7 @@ $sl = $global:ThemeSettings #local settings
 $sl.PromptSymbols.StartSymbol = ''
 $sl.PromptSymbols.PromptIndicator = [char]::ConvertFromUtf32(0x276F)
 $sl.PromptSymbols.SegmentForwardSymbol = [char]::ConvertFromUtf32(0xE0B0)
+$sl.PromptSymbols.UnicodeSpace = [char]::ConvertFromUtf32(0x2000)
 $sl.Colors.PromptForegroundColor = [ConsoleColor]::White
 $sl.Colors.PromptSymbolColor = [ConsoleColor]::White
 $sl.Colors.PromptHighlightColor = [ConsoleColor]::DarkBlue
